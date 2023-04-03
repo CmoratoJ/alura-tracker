ae21f36227378cf3141b1a21252eb5fde0350c13
CmoratoJ <caiomorato5@gmail.com>
2023-04-02 21:50:47 -0300

    feat(app): validando exibição da lista

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,22 +1,24 @@
 <template>
-    <div class="box has-text-weight-bold">
+    <Box>
         <div class="columns">
-            <div class="column is-7">{{ tarefa.descricao }}</div>
+            <div class="column is-7">{{ tarefa.descricao || 'Tarefa sem descrição' }}</div>
             <div class="column">
                 <Cronometro :tempoEmSegundos="tarefa.duracaoEmSegundos"/>
             </div>
         </div>
-    </div>
+    </Box>
 </template>
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 import Cronometro from './Cronometro.vue';
 import ITarefa from '@/interfaces/ITarefa';
+import Box from './Box.vue';
 
 export default defineComponent({
     name: 'Tarefa',
     components: {
-        Cronometro
+        Cronometro,
+        Box
     },
     props: {
         tarefa: {
@@ -26,9 +28,3 @@ export default defineComponent({
     }
 })
 </script>
-
-<style scoped>
-.box {
-    background: #FAF0CA;
-}
-</style>
